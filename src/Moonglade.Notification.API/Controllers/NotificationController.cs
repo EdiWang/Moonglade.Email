0304201ee23466f6da07ce65663ae3ea689c880a
Edi Wang <Edi.Wang@outlook.com>
2019-08-28 13:56:24 +0800

    Add NotificationController

---
+++ src/Moonglade.Notification.API/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Moonglade.Notification.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly ILogger<NotificationController> _logger;
+
+        public NotificationController(ILogger<NotificationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
