d856f83c90496d5eb4dfa4c565f3093ab10dc98e
Edi Wang <Edi.Wang@outlook.com>
2022-04-15 15:25:13 +0800

    code refact

--- src/Moonglade.Notification.AzFunc/EmailHandler.cs
+++ src/Moonglade.Notification.AzFunc/EmailHandler.cs
@@ -1,71 +1,37 @@
 ﻿using Edi.TemplateEmail;
-using Microsoft.Extensions.Logging;
-using MimeKit;
 
 namespace Moonglade.Notification.AzFunc;
 
 public class EmailHandler
 {
-    public IEmailHelper EmailHelper { get; }
+    private readonly string _adminEmail;
+    private readonly IEmailHelper _emailHelper;
 
-    public string EmailDisplayName { get; private init; }
-
-    public string AdminEmail { get; private init; }
-
-    private readonly ILogger _logger;
-
-    public EmailHandler(ILogger logger, string emailDisplayName, string adminEmail)
+    public EmailHandler(IEmailHelper emailHelper, string emailDisplayName, string adminEmail)
     {
-        _logger = logger;
-        EmailDisplayName = emailDisplayName;
-        AdminEmail = adminEmail;
+        _emailHelper = emailHelper;
 
-        var configSource = Path.Join($"{AppDomain.CurrentDomain.GetData(Constants.AppBaseDirectory)}", "mailConfiguration.xml");
-        if (!File.Exists(configSource))
-        {
-            throw new FileNotFoundException("Configuration file for EmailHelper is not present.", configSource);
-        }
+        emailHelper.WithDisplayName(emailDisplayName);
+        emailHelper.WithSenderName(emailDisplayName);
 
-        EmailHelper = new EmailHelper(configSource,
-            Environment.GetEnvironmentVariable("SmtpServer"),
-            Environment.GetEnvironmentVariable("SmtpUserName"),
-            Environment.GetEnvironmentVariable("EmailAccountPassword", EnvironmentVariableTarget.Process),
-            int.Parse(Environment.GetEnvironmentVariable("SmtpServerPort") ?? "587"));
-
-        if (bool.Parse(Environment.GetEnvironmentVariable("EnableSsl") ?? "true")) EmailHelper.WithTls();
-
-        EmailHelper.EmailSent += (sender, eventArgs) =>
-        {
-            if (sender is MimeMessage msg)
-            {
-                _logger.LogInformation($"Email {msg.Subject} is sent, Success: {eventArgs.IsSuccess}");
-            }
-        };
+        _adminEmail = adminEmail;
     }
 
     public async Task SendTestNotificationAsync()
     {
-        _logger.LogInformation("Sending test mail");
-
-        SetEmailInfo();
-
         var pipeline = new TemplatePipeline().Map(nameof(Environment.MachineName), Environment.MachineName)
-            .Map(nameof(EmailHelper.Settings.SmtpServer), EmailHelper.Settings.SmtpServer)
-            .Map(nameof(EmailHelper.Settings.SmtpServerPort), EmailHelper.Settings.SmtpServerPort)
-            .Map(nameof(EmailHelper.Settings.SmtpUserName), EmailHelper.Settings.SmtpUserName)
-            .Map(nameof(EmailHelper.Settings.EmailDisplayName), EmailHelper.Settings.EmailDisplayName)
-            .Map(nameof(EmailHelper.Settings.EnableTls), EmailHelper.Settings.EnableTls);
+            .Map(nameof(EmailHelper.Settings.SmtpServer), _emailHelper.Settings.SmtpServer)
+            .Map(nameof(EmailHelper.Settings.SmtpServerPort), _emailHelper.Settings.SmtpServerPort)
+            .Map(nameof(EmailHelper.Settings.SmtpUserName), _emailHelper.Settings.SmtpUserName)
+            .Map(nameof(EmailHelper.Settings.EmailDisplayName), _emailHelper.Settings.EmailDisplayName)
+            .Map(nameof(EmailHelper.Settings.EnableTls), _emailHelper.Settings.EnableTls);
 
-        await EmailHelper.ApplyTemplate(MailMesageTypes.TestMail.ToString(), pipeline)
-            .SendMailAsync(AdminEmail);
+        await _emailHelper.ApplyTemplate(MailMesageTypes.TestMail.ToString(), pipeline)
+            .SendMailAsync(_adminEmail);
     }
 
     public async Task SendNewCommentNotificationAsync(NewCommentPayload request)
     {
-        _logger.LogInformation("Sending NewCommentNotification mail");
-
-        SetEmailInfo();
-
         var pipeline = new TemplatePipeline().Map(nameof(request.Username), request.Username)
             .Map(nameof(request.Email), request.Email)
             .Map(nameof(request.IpAddress), request.IpAddress)
@@ -73,31 +39,23 @@ public class EmailHandler
             .Map(nameof(request.PostTitle), request.PostTitle)
             .Map(nameof(request.CommentContent), request.CommentContent);
 
-        await EmailHelper.ApplyTemplate(MailMesageTypes.NewCommentNotification.ToString(), pipeline)
-            .SendMailAsync(AdminEmail);
+        await _emailHelper.ApplyTemplate(MailMesageTypes.NewCommentNotification.ToString(), pipeline)
+            .SendMailAsync(_adminEmail);
     }
 
     public async Task SendCommentReplyNotificationAsync(CommentReplyPayload request)
     {
-        _logger.LogInformation("Sending AdminReplyNotification mail");
-
-        SetEmailInfo();
-
         var pipeline = new TemplatePipeline().Map(nameof(request.ReplyContentHtml), request.ReplyContentHtml)
             .Map("RouteLink", request.PostLink)
             .Map("PostTitle", request.Title)
             .Map(nameof(request.CommentContent), request.CommentContent);
 
-        await EmailHelper.ApplyTemplate(MailMesageTypes.AdminReplyNotification.ToString(), pipeline)
+        await _emailHelper.ApplyTemplate(MailMesageTypes.AdminReplyNotification.ToString(), pipeline)
             .SendMailAsync(request.Email);
     }
 
     public async Task SendPingNotificationAsync(PingPayload request)
     {
-        _logger.LogInformation($"Sending BeingPinged mail for post '{request.TargetPostTitle}'");
-
-        SetEmailInfo();
-
         var pipeline = new TemplatePipeline().Map(nameof(request.TargetPostTitle), request.TargetPostTitle)
             .Map(nameof(request.PingTimeUtc), request.PingTimeUtc)
             .Map(nameof(request.Domain), request.Domain)
@@ -105,23 +63,7 @@ public class EmailHandler
             .Map(nameof(request.SourceTitle), request.SourceTitle)
             .Map(nameof(request.SourceUrl), request.SourceUrl);
 
-        await EmailHelper.ApplyTemplate(MailMesageTypes.BeingPinged.ToString(), pipeline)
-            .SendMailAsync(AdminEmail);
-    }
-
-    private void SetEmailInfo()
-    {
-        if (string.IsNullOrWhiteSpace(EmailDisplayName))
-        {
-            throw new ArgumentNullException(nameof(EmailDisplayName));
-        }
-
-        if (string.IsNullOrWhiteSpace(AdminEmail))
-        {
-            throw new ArgumentNullException(nameof(AdminEmail));
-        }
-
-        EmailHelper.WithDisplayName(EmailDisplayName);
-        EmailHelper.WithSenderName(EmailDisplayName);
+        await _emailHelper.ApplyTemplate(MailMesageTypes.BeingPinged.ToString(), pipeline)
+            .SendMailAsync(_adminEmail);
     }
 }
--- src/Moonglade.Notification.AzFunc/EmailSendingFunction.cs
+++ src/Moonglade.Notification.AzFunc/EmailSendingFunction.cs
@@ -1,8 +1,10 @@
 using System.Text.Json;
+using Edi.TemplateEmail;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using MimeKit;
 using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
 namespace Moonglade.Notification.AzFunc;
@@ -24,29 +26,49 @@ public class EmailSendingFunction
 
         try
         {
-            var configRootDirectory = executionContext.FunctionAppDirectory;
-            AppDomain.CurrentDomain.SetData(Constants.AppBaseDirectory, configRootDirectory);
-            log.LogInformation($"Function App Directory: {configRootDirectory}");
+            var configSource = Path.Join(executionContext.FunctionAppDirectory, "mailConfiguration.xml");
+            if (!File.Exists(configSource)) throw new FileNotFoundException("Configuration file for EmailHelper is not present.", configSource);
 
-            var notification = new EmailHandler(log, request.AdminEmail, request.EmailDisplayName);
+            var emailHelper = new EmailHelper(
+                configSource,
+                Environment.GetEnvironmentVariable("SmtpServer"),
+                Environment.GetEnvironmentVariable("SmtpUserName"),
+                Environment.GetEnvironmentVariable("EmailAccountPassword", EnvironmentVariableTarget.Process),
+                int.Parse(Environment.GetEnvironmentVariable("SmtpServerPort") ?? "587"));
+
+            if (bool.Parse(Environment.GetEnvironmentVariable("EnableSsl") ?? "true")) emailHelper.WithTls();
+
+            emailHelper.EmailSent += (sender, eventArgs) =>
+            {
+                if (sender is MimeMessage msg)
+                {
+                    log.LogInformation($"Email '{msg.Subject}' is sent. Success: {eventArgs.IsSuccess}");
+                }
+            };
+
+            var notification = new EmailHandler(emailHelper, request.AdminEmail, request.EmailDisplayName);
 
             switch (request.MessageType)
             {
                 case MailMesageTypes.TestMail:
+                    log.LogInformation("Sending test mail");
                     await notification.SendTestNotificationAsync();
                     return new OkObjectResult("TestMail Sent");
 
                 case MailMesageTypes.NewCommentNotification:
+                    log.LogInformation("Sending NewCommentNotification mail");
                     var commentPayload = GetModelFromPayload<NewCommentPayload>();
                     _ = Task.Run(async () => await notification.SendNewCommentNotificationAsync(commentPayload));
                     return new OkObjectResult("NewCommentNotification Sent");
 
                 case MailMesageTypes.AdminReplyNotification:
+                    log.LogInformation("Sending AdminReplyNotification mail");
                     var replyPayload = GetModelFromPayload<CommentReplyPayload>();
                     _ = Task.Run(async () => await notification.SendCommentReplyNotificationAsync(replyPayload));
                     return new OkObjectResult("AdminReplyNotification Sent");
 
                 case MailMesageTypes.BeingPinged:
+                    log.LogInformation($"Sending BeingPinged mail");
                     var pingPayload = GetModelFromPayload<PingPayload>();
                     _ = Task.Run(async () => await notification.SendPingNotificationAsync(pingPayload));
                     return new OkObjectResult("BeingPinged Sent");
