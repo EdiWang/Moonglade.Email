5c7898f4590dac873f6d21f3a86ee6a49498a304
Edi Wang <edi.wang@outlook.com>
2022-09-08 15:17:58 +0800

    db based notifictaion

---
+++ src/Moonglade.Notification.AzFunc/EmailNotification.cs
@@ -0,0 +1,15 @@
+﻿namespace Moonglade.Notification.AzFunc;
+
+public class EmailNotification
+{
+    public Guid Id { get; set; }
+
+    public string DistributionList { get; set; }
+    public string MessageType { get; set; }
+    public string MessageBody { get; set; }
+    public int SendingStatus { get; set; }
+    public DateTime CreateTimeUtc { get; set; }
+    public DateTime SentTimeUtc { get; set; }
+    public string TargetResponse { get; set; }
+    public int RetryCount { get; set; }
+}
