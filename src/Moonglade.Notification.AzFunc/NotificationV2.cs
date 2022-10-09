1c473ae85f3066dcdd7ae0d8ef96590087836832
Edi Wang <edi.wang@outlook.com>
2022-10-09 10:01:38 +0800

    Update NotificationV2.cs

--- src/Moonglade.Notification.AzFunc/NotificationV2.cs
+++ src/Moonglade.Notification.AzFunc/NotificationV2.cs
@@ -177,7 +177,7 @@ public class EmailNotification
     public string MessageBody { get; set; }
     public int SendingStatus { get; set; }
     public DateTime CreateTimeUtc { get; set; }
-    public DateTime SentTimeUtc { get; set; }
+    public DateTime? SentTimeUtc { get; set; }
     public string TargetResponse { get; set; }
     public int RetryCount { get; set; }
 }
