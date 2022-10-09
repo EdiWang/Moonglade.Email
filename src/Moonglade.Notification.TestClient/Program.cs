4678c46e6ba599bd2194a681f434e0b1eb925097
Edi Wang <edi.wang@outlook.com>
2022-10-09 10:03:50 +0800

    azure storage queue test client

---
+++ src/Moonglade.Notification.TestClient/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text.Json;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace Moonglade.Notification.TestClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter Azure Storage connection string");
+        var connectionString = Console.ReadLine();
+
+        Console.WriteLine("Enter Queue name");
+        var queueName = Console.ReadLine();
+
+        Console.WriteLine("Enter test email receiver addresss");
+        var dl = Console.ReadLine();
+
+        var queue = new QueueClient(connectionString, queueName);
+
+        var testEmail = new EmailNotification
+        {
+            DistributionList = dl,
+            MessageBody = string.Empty,
+            MessageType = "TestMail",
+            RetryCount = 0,
+            SendingStatus = 1
+        };
+
+        await InsertMessageAsync(queue, testEmail);
+
+        Console.WriteLine("Press any key to retrive next message");
+        Console.ReadLine();
+        var message = await RetrieveNextMessageAsync(queue);
+
+        var json = JsonSerializer.Serialize(message, new JsonSerializerOptions { WriteIndented = true });
+        Console.WriteLine(json);
+
+        Console.WriteLine("Press any key to delete this message");
+        Console.ReadLine();
+        await DeleteMessage(queue, message);
+    }
+
+    static async Task InsertMessageAsync(QueueClient theQueue, EmailNotification emailNotification)
+    {
+        if (null != await theQueue.CreateIfNotExistsAsync())
+        {
+            Console.WriteLine("The queue was created.");
+        }
+
+        var json = JsonSerializer.Serialize(emailNotification);
+        await theQueue.SendMessageAsync(json);
+
+        Console.WriteLine($"Inserted message: {json}");
+    }
+
+    static async Task<QueueMessage> RetrieveNextMessageAsync(QueueClient theQueue)
+    {
+        if (await theQueue.ExistsAsync())
+        {
+            QueueProperties properties = await theQueue.GetPropertiesAsync();
+
+            if (properties.ApproximateMessagesCount > 0)
+            {
+                QueueMessage[] retrievedMessage = await theQueue.ReceiveMessagesAsync(1);
+                var theMessage = retrievedMessage[0];
+                return theMessage;
+            }
+        }
+
+        return null;
+    }
+
+    static async Task DeleteMessage(QueueClient theQueue, QueueMessage message)
+    {
+        var response = await theQueue.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+        Console.WriteLine($"DeleteMessage Response: {response.Status}:{response.ReasonPhrase}");
+    }
+}
+
+public class EmailNotification
+{
+    public string DistributionList { get; set; }
+    public string MessageType { get; set; }
+    public string MessageBody { get; set; }
+    public int SendingStatus { get; set; }
+    public DateTime? SentTimeUtc { get; set; }
+    public string TargetResponse { get; set; }
+    public int RetryCount { get; set; }
+}
