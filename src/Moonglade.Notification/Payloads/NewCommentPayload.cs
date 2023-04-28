896d0fb5dd4fa00437d174e24e6ca44cebe6e59b
Edi Wang <Edi.Wang@outlook.com>
2023-04-28 20:07:42 +0800

    test .net 7

---
+++ src/Moonglade.Notification/Payloads/NewCommentPayload.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonglade.Notification.Payloads;
+
+public class NewCommentPayload
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string IpAddress { get; set; }
+
+    [Required]
+    public string PostTitle { get; set; }
+
+    [Required]
+    public string CommentContent { get; set; }
+}
---
+++ src/Moonglade.Notification/Payloads/PingPayload.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moonglade.Notification.Payloads;
+
+public class PingPayload
+{
+    [Required]
+    public string TargetPostTitle { get; set; }
+
+    [Required]
+    public string Domain { get; set; }
+
+    [Required]
+    public string SourceIp { get; set; }
+
+    [Required]
+    public string SourceUrl { get; set; }
+
+    [Required]
+    public string SourceTitle { get; set; }
+}
