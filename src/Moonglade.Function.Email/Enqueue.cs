3d84fb8c877391a8801b71c3e1a25e58ec8da7b3
Edi Wang <Edi.Wang@outlook.com>
2024-01-21 13:10:08 +0800

    Update Enqueue.cs

--- src/Moonglade.Function.Email/Enqueue.cs
+++ src/Moonglade.Function.Email/Enqueue.cs
@@ -51,5 +51,5 @@ public class EnqueueRequest
     public string Type { get; set; }
     public string[] Receipts { get; set; }
     public object Payload { get; set; }
-    public string OriginAspNetRequestId { get; set; }
+    public string OriginAspNetRequestId { get; set; } = Guid.Empty.ToString();
 }
