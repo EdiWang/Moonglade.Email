87c045c183f0ce3e1924a24d8c1aba2cf1151fa6
Edi Wang <Edi.Wang@outlook.com>
2024-10-13 09:57:58 +0800

    Integrate Azure Communication Services Email SDK
    Updated AzureCommunicationSender to use Azure Communication Services Email SDK. The SendAsync method now returns an EmailSendOperation and includes logic to send an email using the EmailClient. Environment variables are used for the connection string and sender address. Added package reference for Azure.Communication.Email version 1.0.1 in Moonglade.Function.Email.csproj.

--- src/Moonglade.Function.Email/AzureCommunicationSender.cs
+++ src/Moonglade.Function.Email/AzureCommunicationSender.cs
@@ -1,12 +1,34 @@
-﻿using Edi.TemplateEmail;
+﻿using Azure;
+using Azure.Communication.Email;
+using Edi.TemplateEmail;
 
 namespace Moonglade.Function.Email;
 
 public class AzureCommunicationSender
 {
-    public async Task SendAsync(CommonMailMessage message)
+    public async Task<EmailSendOperation> SendAsync(CommonMailMessage message)
     {
-        throw new NotImplementedException();
+        var connectionString = EnvHelper.Get<string>("AzureCommunicationConnection");
+        var senderAddress = EnvHelper.Get<string>("AzureCommunicationSenderAddress");
+
+        var emailClient = new EmailClient(connectionString);
+
+        var emailMessage = new EmailMessage(
+            senderAddress: senderAddress,
+            content: new EmailContent(message.Subject),
+            recipients: new EmailRecipients(message.Receipts.Select(p => new EmailAddress(p))));
+
+        if (message.BodyIsHtml)
+        {
+            emailMessage.Content.Html = message.Body;
+        }
+        else
+        {
+            emailMessage.Content.PlainText = message.Body;
+        }
+
+        var emailSendOperation = await emailClient.SendAsync(WaitUntil.Completed, emailMessage);
+        return emailSendOperation;
     }
 }
 
